17bea6083af05cebfe461d804407316fe8d0ccbe
MetatronL <pnbgdn@protonmail.com>
2017-11-23 14:58:47 +0200

    7 1

--- lab7/ex1.c
+++ lab7/ex1.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <errno.h>
+#include <pthread.h>
+#include <stdlib.h>
 
 const int maxResources = 5;
-int resources = maxResources;
+int resources = 5;
 
 int decrease_count(int count){
 	if( resources < count )
@@ -22,33 +24,51 @@ pthread_mutex_t mtx;
 
 void* tryToReserve(void *arg){
 	int count = *((int*)arg);
-	while(true){
+	if( count > maxResources ){
+		printf("Trying to allocate more resources(%d) than total\n",count);
+		fflush(stdout);
+		return 0;
+	}
+	while( 1 ){
 		pthread_mutex_lock(  &mtx);
-		if( decrease_count( count ) == -1 )
+		if( decrease_count( count ) == -1 ){
+			pthread_mutex_unlock(  &mtx);
 			continue;
+		}
 		printf("Got %d resources %d remaining\n",count,resources);
+		fflush(stdout);
 		int i = 0 ;
-		for( i = 0; i < count;++i)  ;
-		pthread_mutex_unlock(  &mtx);
+		for( i = 0; i <  count;++i)  ;
+		increase_count( count);
+		printf("Released %d resources %d remaining\n",count,resources);
+		fflush(stdout);
 		
-	}	
+		break;
+		
+	}
+	
+	pthread_mutex_unlock(  &mtx);
 }
 
 int main(int argc, char *argv[]){
 	
-	if( pthread_mutex_init( &mtx, NULL){
+	if( pthread_mutex_init( &mtx, NULL) ){
 		perror(NULL);
 		return errno;
 	}
 
-	int v[] = {3,2,3,2,4,1,3,4)
-	int n = sizeof(v)/sizeof(n);
+	int v[] = {3,2,3,2,5,4,1,3,4};
+	int n = sizeof(v)/sizeof(int);
 	
 	int i;
+	pthread_t *thm = (pthread_t*)malloc(n*sizeof(pthread_t) ) ;
 	for(i=0;i<n;++i){
-		
+		pthread_create( &thm[i] , NULL , tryToReserve , &v[i]);
 	}
 	
+	for(int i=0;i<n;++i){
+		pthread_join(thm[i] , NULL) ; 
+	}
 		
 	
 	
